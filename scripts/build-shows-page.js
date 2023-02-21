a50490c6dfb79cdc85bf4bc2df8c7e497ec06124
Richard Chiu <richard.chiu87@gmail.com>
2023-02-21 13:38:20 -0500

    added axios script, shows data hard code adjusted for with api data

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,133 +1,117 @@
 const shows = document.querySelector(".shows");
+const url = "https://project-1-api.herokuapp.com/showdates?api_key=";
+const apiKey = "20ccaf53-7f6b-4cc3-ab19-4f1f97db6b49";
+const getRes = axios.get(url + `${apiKey}`);
 
-// storing data in an array of objects
-const showsData = [
-  {
-    Date: "Mon Sept 06 2021",
-    Venue: "Ronald Lane",
-    Location: "San Francisco, CA",
-  },
-  {
-    Date: "Tue Sept 21 2021",
-    Venue: "Pier 3 East",
-    Location: "San Francisco, CA",
-  },
-  {
-    Date: "Fri Oct 15 2021",
-    Venue: "View Lounge",
-    Location: "San Francisco, CA",
-  },
-  {
-    Date: "Sat Nov 06 2021",
-    Venue: "Hyatt Agency",
-    Location: "San Francisco, CA",
-  },
-  {
-    Date: "Fri Nov 26 2021",
-    Venue: "Moscow Center",
-    Location: "San Francisco, CA",
-  },
-  {
-    Date: "Wed Dec 15 2021",
-    Venue: "Press Club",
-    Location: "San Francisco, CA",
-  },
-];
-
-// creating shows DOM element
-function createShowsElement(Data) {
-  const showsMain = document.createElement("div");
-  showsMain.classList.add("shows__main");
-
-  const showsInformationContainerDate = document.createElement("div");
-  showsInformationContainerDate.classList.add("shows__information-container");
-  showsInformationContainerDate.classList.add(
-    "shows__information-container--date"
-  );
-
-  const showsInformationContainerVenue = document.createElement("div");
-  showsInformationContainerVenue.classList.add("shows__information-container");
-  showsInformationContainerVenue.classList.add(
-    "shows__information-container--venue"
-  );
-
-  const showsInformationContainerLocation = document.createElement("div");
-  showsInformationContainerLocation.classList.add(
-    "shows__information-container"
-  );
-  showsInformationContainerLocation.classList.add(
-    "shows__information-container--location"
-  );
-
-  const showsDetailsDate = document.createElement("p");
-  showsDetailsDate.classList.add("shows__details");
-  showsDetailsDate.classList.add("shows__details--date");
-  showsDetailsDate.innerText = "Date";
-
-  const showsDetailsVenue = document.createElement("p");
-  showsDetailsVenue.classList.add("shows__details");
-  showsDetailsVenue.classList.add("shows__details--venue");
-  showsDetailsVenue.innerText = "Venue";
-
-  const showsDetailsLocation = document.createElement("p");
-  showsDetailsLocation.classList.add("shows__details");
-  showsDetailsLocation.classList.add("shows__details--location");
-  showsDetailsLocation.innerText = "Location";
-
-  const showsInfoDate = document.createElement("p");
-  showsInfoDate.classList.add("shows__info");
-  showsInfoDate.classList.add("shows__info--date");
-  showsInfoDate.innerText = Data.Date;
-
-  const showsInfoVenue = document.createElement("p");
-  showsInfoVenue.classList.add("shows__info");
-  showsInfoVenue.classList.add("shows__info--venue");
-  showsInfoVenue.innerText = Data.Venue;
-
-  const showsInfoLocation = document.createElement("p");
-  showsInfoLocation.classList.add("shows__info");
-  showsInfoLocation.classList.add("shows__info--location");
-  showsInfoLocation.innerText = Data.Location;
-
-  const showsButton = document.createElement("button");
-  showsButton.classList.add("shows__button");
-  showsButton.innerText = "BUY TICKETS";
-
-  shows.append(showsMain);
-  showsMain.append(showsInformationContainerDate);
-  showsInformationContainerDate.append(showsDetailsDate);
-  showsInformationContainerDate.append(showsInfoDate);
-  showsMain.append(showsInformationContainerVenue);
-  showsInformationContainerVenue.append(showsDetailsVenue);
-  showsInformationContainerVenue.append(showsInfoVenue);
-  showsMain.append(showsInformationContainerLocation);
-  showsInformationContainerLocation.append(showsDetailsLocation);
-  showsInformationContainerLocation.append(showsInfoLocation);
-  showsMain.append(showsButton);
-
-  if (showsData[0] === Data) {
-    showsDetailsDate.classList.add("shows__details--visible");
-    showsDetailsVenue.classList.add("shows__details--visible");
-    showsDetailsLocation.classList.add("shows__details--visible");
-    showsButton.classList.add("shows__button-top");
-  }
-  return showsMain;
+//function for converting timestamp into string date
+function stringDate(dateCode){
+  const date = new Date(dateCode);
+  const dateString = date.toLocaleDateString();
+  return dateString
 }
+getRes.then((response) => {
+  const apiData = response.data;
+  console.log(apiData);
 
-// populating elements for the showsData array
-showsData.forEach((i) => {
-  createShowsElement(i);
+  // creating shows DOM element
+  function createShowsElement(apiData) {
+    const showsMain = document.createElement("div");
+    showsMain.classList.add("shows__main");
+
+    const showsInformationContainerDate = document.createElement("div");
+    showsInformationContainerDate.classList.add("shows__information-container");
+    showsInformationContainerDate.classList.add(
+      "shows__information-container--date"
+    );
+
+    const showsInformationContainerVenue = document.createElement("div");
+    showsInformationContainerVenue.classList.add(
+      "shows__information-container"
+    );
+    showsInformationContainerVenue.classList.add(
+      "shows__information-container--venue"
+    );
+
+    const showsInformationContainerLocation = document.createElement("div");
+    showsInformationContainerLocation.classList.add(
+      "shows__information-container"
+    );
+    showsInformationContainerLocation.classList.add(
+      "shows__information-container--location"
+    );
+
+    const showsDetailsDate = document.createElement("p");
+    showsDetailsDate.classList.add("shows__details");
+    showsDetailsDate.classList.add("shows__details--date");
+    showsDetailsDate.innerText = "Date";
+
+    const showsDetailsVenue = document.createElement("p");
+    showsDetailsVenue.classList.add("shows__details");
+    showsDetailsVenue.classList.add("shows__details--venue");
+    showsDetailsVenue.innerText = "Venue";
+
+    const showsDetailsLocation = document.createElement("p");
+    showsDetailsLocation.classList.add("shows__details");
+    showsDetailsLocation.classList.add("shows__details--location");
+    showsDetailsLocation.innerText = "Location";
+
+    const showsInfoDate = document.createElement("p");
+    showsInfoDate.classList.add("shows__info");
+    showsInfoDate.classList.add("shows__info--date");
+    showsInfoDate.innerText = stringDate(apiData.date);
+
+    const showsInfoVenue = document.createElement("p");
+    showsInfoVenue.classList.add("shows__info");
+    showsInfoVenue.classList.add("shows__info--venue");
+    showsInfoVenue.innerText = apiData.place;
+
+    const showsInfoLocation = document.createElement("p");
+    showsInfoLocation.classList.add("shows__info");
+    showsInfoLocation.classList.add("shows__info--location");
+    showsInfoLocation.innerText = apiData.location;
+
+    const showsButton = document.createElement("button");
+    showsButton.classList.add("shows__button");
+    showsButton.innerText = "BUY TICKETS";
+
+    shows.append(showsMain);
+    showsMain.append(showsInformationContainerDate);
+    showsInformationContainerDate.append(showsDetailsDate);
+    showsInformationContainerDate.append(showsInfoDate);
+    showsMain.append(showsInformationContainerVenue);
+    showsInformationContainerVenue.append(showsDetailsVenue);
+    showsInformationContainerVenue.append(showsInfoVenue);
+    showsMain.append(showsInformationContainerLocation);
+    showsInformationContainerLocation.append(showsDetailsLocation);
+    showsInformationContainerLocation.append(showsInfoLocation);
+    showsMain.append(showsButton);
+
+    // for selecting first data entry to add classes
+    if (apiData[0] === apiData) {
+      showsDetailsDate.classList.add("shows__details--visible");
+      showsDetailsVenue.classList.add("shows__details--visible");
+      showsDetailsLocation.classList.add("shows__details--visible");
+      showsButton.classList.add("shows__button-top");
+    }
+    return showsMain;
+  }
+
+  // populating data for each api data entry 
+  apiData.forEach((i) => {
+    createShowsElement(i);
+  });
 });
 
 // Selecting the containers needed to apply active class
 const showsMainAll = document.querySelectorAll(".shows__main");
 const showsMain = document.querySelector(".shows__main");
-const showsButtonAll = document.querySelectorAll('.shows__button')
-const showsButton = document.querySelector('.shows__button')
+const showsButtonAll = document.querySelectorAll(".shows__button");
+const showsButton = document.querySelector(".shows__button");
 const showsDesktop = document.createElement("div");
 showsDesktop.classList.add("shows__desktop-div");
 shows.append(showsDesktop);
 console.log(showsButton);
+
 // this variable is to keep track of which show div is active
 let activeShowsMain = null;
 
